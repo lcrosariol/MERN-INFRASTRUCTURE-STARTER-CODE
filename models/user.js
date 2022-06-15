5f001235bbd7adb9e0ef999777ba7f2e06c9c990
lauren <laurencrosariol@yahoo.com>
2022-06-15 00:03:49 -0700

    initial commit

--- models/user.js
+++ models/user.js
@@ -1,40 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
-const SALT_ROUNDS = 6;  // 6 is a reasonable value
+const SALT_ROUNDS = 6; // 6 is a reasonable value
 
-const userSchema = new Schema({
-        name: {type: String, required: true},
+const userSchema = new Schema(
+    {
+        name: { type: String, required: true },
         email: {
-            type: String,
-            unique: true,
-            trim: true,
-            lowercase: true,
-            required: true
+        type: String,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        required: true,
         },
         password: {
-            type: String,
-            trim: true,
-            minLength: 3,
-            required: true
-        }
+        type: String,
+        trim: true,
+        minLength: 3,
+        required: true,
+        },
     },
     {
         timestamps: true,
+        // Even though it's hashed - don't serialize the password
         toJSON: {
-            transform: function (doc, ret) {
-                delete ret.password;
-                return ret;
-            }
-        }
+        transform: function (doc, ret) {
+            delete ret.password;
+            return ret;
+        },
+        },
     }
 );
 
-userSchema.pre('save', function (next) {
+userSchema.pre("save", function (next) {
     // Save the reference to the user doc
     const user = this;
-    if (!user.isModified('password')) return next();
+    if (!user.isModified("password")) return next();
     // password has been changed - salt and hash it
     bcrypt.hash(user.password, SALT_ROUNDS, function (err, hash) {
         if (err) return next(err);
@@ -44,4 +46,4 @@ userSchema.pre('save', function (next) {
     });
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,5 +1,12 @@
-export default function NewOrderPage() {
+export default function NewOrderPage({categories}) {
     return (
-        <h1>NewOrderPage</h1>
+        <>
+            <h1>NewOrderPage</h1>
+                <div >
+                {/* {categories.map((category) => (
+                    <div>{category.name}</div>
+                ))} */}
+                </div>
+        </>
     );
 }
